1a172a67373f5e35e7b0dd48d0cf72e6102e8542
tabamotch <tabamotch+git@gmail.com>
2018-12-06 19:27:37 +0900

    MySampleVisitorを追加 等

---
+++ ScriptDomSampleConsole/MySampleVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace ScriptDomSampleConsole
+{
+    public class MySampleVisitor : TSqlFragmentVisitor
+    {
+        private string _sql;
+
+        public MySampleVisitor(string sql) => _sql = sql;
+
+        public override void Visit(QuerySpecification node)
+        {
+
+        }
+
+        public override void ExplicitVisit(QuerySpecification node)
+        {
+
+        }
+    }
+}
--- ScriptDomSampleConsole/Program.cs
+++ ScriptDomSampleConsole/Program.cs
@@ -4,12 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
 namespace ScriptDomSampleConsole
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string SQL = @"SELECT
+    a.*,
+    e.column_c
+FROM
+    TableA AS a
+    INNER JOIN
+    TableB AS b ON
+        b.column_a = a.column_a
+    AND b.b = a.column_b
+    AND b.c = a.column_c
+    AND b.column_d = a.column_d
+    AND b.column_e = a.column_e
+    INNER JOIN
+    (
+    SELECT
+        c.column_a,
+        c.column_b,
+        d.column_c
+    FROM
+        TableC AS c
+        LEFT OUTER JOIN
+        TableD AS d ON
+            d.column_a = c.column_a
+        AND d.column_b = c.column_b
+        AND d.column_f <> '3'
+    ) AS e ON
+        e.column_a = a.column_a
+    AND e.column_b = a.column_b
+WHERE
+        a.column_a = @column_a
+    AND (a.column_f < '3' OR a.column_f > '3')";
+
+        public static void Main(string[] args)
         {
+            TSqlFragment parseResult;
+            IList<ParseError> errors;
+
+            TSql100Parser parser = new TSql100Parser(true);
+            using (TextReader reader = new StringReader(SQL))
+            {
+                parseResult = parser.Parse(reader, out errors);
+            }
+
+            if (errors.Count > 0)
+            {
+                foreach (ParseError errorItem in errors)
+                {
+                    Console.Error.WriteLine(errorItem.Message);
+                }
+                return;
+            }
+
+            MySampleVisitor visitor = new MySampleVisitor(SQL);
+            parseResult.Accept(visitor);
         }
     }
 }
